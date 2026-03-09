2c66b4cbea190d49eb7988d101764bf5a28202db
Derjen Frick <derjenyt@gmail.com>
2026-03-09 11:41:13 +0100

    Added route styles to index.css

--- frontend/components/NavBar/NavBar.jsx
+++ frontend/components/NavBar/NavBar.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom"
-
+import './navbar.css'
 export default function NavBar()
 {
     return(
         <section>
-            <nav>
+            <nav className="nav-links">
                 <NavLink to='/home'>Home</NavLink>
+                <NavLink to='/login'>Login</NavLink>
             </nav>
         </section>
     )
