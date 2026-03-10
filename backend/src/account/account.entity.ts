fbebe337892338a8bde6a5b837aa7ee10963e6e8
FlaviusPetrasciuc <flavius.petrasciuc@student.nhlstenden.com>
2026-03-10 20:18:05 +0100

    Created the account directory in src and the account.entity.ts file

---
+++ backend/src/account/account.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'account' })
+export class Account {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'name', type: 'varchar', length: 100  })
+  name: string;
+
+  @Column({ name: 'surname', type: 'varchar', length: 100 })
+  surname: string;
+
+  @Column({ name: 'email', type: 'varchar', length: 255, unique: true })
+  email: string;
+
+  @Column({ name: 'password', type: 'varchar', length: 255 })
+  password: string;
+}
