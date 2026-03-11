e74d75a7d20b23b2a41ff6e21fb81f2b296cefe6
Derjen Frick <derjenyt@gmail.com>
2026-03-11 14:18:02 +0100

    Fixed front end

--- frontend/pages/ResetPassword/ResetPassword.jsx
+++ frontend/pages/ResetPassword/ResetPassword.jsx
@@ -1,28 +1,24 @@
+
 import { Link } from "react-router-dom";
-import "../src/styles/auth.css";
+import "../../src/styles/auth.css";
 
 function ResetPassword() {
+
+
   return (
     <div className="auth-page">
-
       <div className="logo">IT Hub Logo</div>
-
       <div className="auth-card">
         <h2>Reset Password</h2>
-
         <div className="info-box">
           Enter your email address and we'll send you a link to reset your password.
         </div>
-
         <label>Email</label>
         <input type="email" placeholder="Enter your email" />
-
         <button className="primary-btn">
           Send Reset Link
         </button>
-
         <Link className="back-link" to="/login">Back to Login</Link>
-
       </div>
     </div>
   );
